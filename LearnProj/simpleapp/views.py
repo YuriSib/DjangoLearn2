34b8abf90ba1d008c569e955f86f94aee4be2761
YuriSib <sib_94@vk.com>
2024-02-03 09:28:39 +0300

    Учебный проект интернет-магазина. На данном этапе реализованы: • Модели БД; • Подтянут шаблон-html; • Представления

---
+++ LearnProj/simpleapp/views.py
@@ -0,0 +1,25 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+
+    # queryset = Product.objects.filter(
+    #     price__lt=300
+    # ).order_by('-name')
+
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
