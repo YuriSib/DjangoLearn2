228dd449ef87b167622f1dcdbd155b8aa2f6d484
YuriSib <sib_94@vk.com>
2024-02-03 19:38:33 +0300

    Учебный проект интернет-магазина. На данном этапе реализованы: • установил html-шаблон; • сделал представление, с фильтрацией только по новостям для объекта класса Post; • написал собственный фильтр нецензурных слов

--- LearnProj/simpleapp/views.py
+++ LearnProj/simpleapp/views.py
@@ -1,5 +1,7 @@
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
+from datetime import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -9,11 +11,26 @@ class ProductsList(ListView):
     ordering = 'name'
 
     # queryset = Product.objects.filter(
-    #     price__lt=300
+    #     price__lt=100
     # ).order_by('-name')
 
     template_name = 'products.html'
     context_object_name = 'products'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        # context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        return context
 
 
 class ProductDetail(DetailView):
